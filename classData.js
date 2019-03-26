ce7c3b5a49ccd9f0571807a420cf321b5e943012
CrazyMKing <CrazyMKing@CrazyMKing-2.local>
2019-03-26 14:43:12 -0400

    s

--- classData.js
+++ classData.js
@@ -7,3 +7,30 @@ function(err)
 {
   console.log(err);
 })
+
+var drawHistogram=function(data)
+{
+  var scren=
+  {
+    width=800
+    height=600
+  };
+  var svg=d3.select("svg")
+            .attr("width",scren.width)
+            .attr("height",scren.height)
+  var margins=
+  {
+    top:20,
+    bottom:30,
+    left:35,
+    right:70
+  }
+  var width=scren.width-margins.left-margins.right;
+  var height=scren.height-margins.top-margins.bottom;
+
+  var xScale=d3.scaleLinear()
+               .domain([d3.extend(data)])
+               .nice()
+               .range([0,width]);
+
+}
